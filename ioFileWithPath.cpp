ac8991ee5c55efee7356469e0332401cc509a103
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-06-10 10:45:59 +0700

    add file reading logic to display contents after writing

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -37,5 +37,15 @@ int main() {
 
     cout << endl << ">= Membuka dan Membaca file " << endl;
     // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris)) {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
 
 }
