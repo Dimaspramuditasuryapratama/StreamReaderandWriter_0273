208da8ae9f078da6f5b8dd727b707089e39ac0ab
Dimaspramuditasuryapratama <dimaspramuditas.pgmail.com>
2025-06-10 10:40:44 +0700

    implement file writing loop to capture user input until 'q' is entered

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -12,5 +12,20 @@ int main() {
 
     // membuat file dalam mode menulis.
     ofstream outfile;
+    // menunjuk ke sebuah nama file.
+    outfile.open(NamaFile + ".text", ios::out);
+    
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    // unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        // loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
  
 }
